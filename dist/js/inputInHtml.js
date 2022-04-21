838268426b91cf9d4c2f968154824c57e2ad73e2
sharull9 <arafatulla121@gmail.com>
2022-04-22 00:34:37 +0530

    remove extra bootstrap files

--- dist/js/inputInHtml.js
+++ dist/js/inputInHtml.js
@@ -1,10 +1,12 @@
 const mobileMenu = document.querySelector('#mobile-menu-list')
 const desktopMenu = document.querySelector('#desktop-menu-list')
+const footerMenu = document.querySelector('#footer-menu-list')
 
-const listItems = `<li><a href="#">About</a></li>
-<li><a href="#">Work</a></li>
-<li><a href="#">Blog</a></li>
-<li><a href="#">Contact</a></li>`;
+const listItems = 
+`<li><a href="#about">About</a></li>
+<li><a href="#work">Work</a></li>
+<li><a href="#blog">Blog</a></li>
+<li><a href="#contact">Contact</a></li>`;
 
 mobileMenu.innerHTML = listItems;
 desktopMenu.innerHTML = listItems;
