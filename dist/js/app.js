c88020ece7c8d96e5be2d63f0c4cd3f4f9362760
sharull9 <arafatulla121@gmail.com>
2022-04-21 19:36:18 +0530

    Initial commit

---
+++ dist/js/app.js
@@ -0,0 +1,14 @@
+const menuBtn = document.querySelector('.mobile-menu-btn');
+const header = document.querySelector('header');
+
+
+menuBtn.addEventListener('click', ()=>{
+    if(!header.classList.contains('active')){
+        header.classList.add('active');
+        header.classList.remove('close');
+    }
+    else{
+        header.classList.remove('active');
+        header.classList.add('close');
+    }
+})
