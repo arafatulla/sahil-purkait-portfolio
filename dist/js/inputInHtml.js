d7c5e38b2877bbb3ae89474f9742fc1fd36c121d
sharull9 <arafatulla121@gmail.com>
2022-05-29 21:13:24 +0530

    main

--- dist/js/inputInHtml.js
+++ dist/js/inputInHtml.js
@@ -5,11 +5,12 @@ const footerMenu = document.querySelector('#footer-menu-list')
 const listItems = 
 `<li><a href="#about">About</a></li>
 <li><a href="#work">Work</a></li>
-<li><a href="#blog">Blog</a></li>
+<li><a href="https://sharull9.blogspot.com">Blog</a></li>
 <li><a href="#contact">Contact</a></li>`;
 
 mobileMenu.innerHTML = listItems;
 desktopMenu.innerHTML = listItems;
+
 
 
 
